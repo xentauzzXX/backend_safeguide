ec5001f4429948a4d90c1811da118d17f63aef10
siredenta <89696526+xentauzzyam@users.noreply.github.com>
2024-12-16 20:09:35 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,17 @@
 <?php
 
-// Dapatkan autoloader Laravel
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap aplikasi Laravel
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// Jalankan aplikasi
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
